dc8db43befdf7999d5d602c18d79643c4c0327bb
Chanita Anuwong <chanita@odd-e.com>
2014-09-17 14:01:25 +0700

    - ban2stats clients and its test

---
+++ client/client.py
@@ -0,0 +1,22 @@
+from django.conf import settings
+from django.shortcuts import render_to_response
+from django.template.context import RequestContext
+from models import RedirectServer
+from httplib2 import Http
+from urllib import urlencode
+from urlparse import urljoin
+
+
+class BAN2STATSHandler(object):
+
+    WEBSERVICE_HOST = 'http://localhost:8000'
+    WEBSERVICE_HANDLER_URL = '/'
+
+    def call_webservice_api(self):
+        target_url = urljoin(self.WEBSERVICE_HOST, self.WEBSERVICE_HANDLER_URL)
+
+        http = Http()
+        headers = {'Content-type': 'application/x-www-form-urlencoded'}
+        data = {}
+        body = urlencode(data)
+        response, content = http.request(target_url, "POST", headers=headers, body=body)
