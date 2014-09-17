f3b98075f238ed7b51b445fa7e6a17bd1faa98d3
Chanita Anuwong <chanita@odd-e.com>
2014-09-17 15:33:41 +0700

    - simple page for integration testing purpose

--- ban2stats/urls.py
+++ ban2stats/urls.py
@@ -2,8 +2,7 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'ban2stats.views.home', name='home'),
+    url(r'^$', 'ban2stats.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     # url(r'^admin/', include(admin.site.urls)),
