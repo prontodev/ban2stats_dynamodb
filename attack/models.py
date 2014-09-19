e9fb36d2868b1cac1ad8475b6dcf1af9d2610492
Chanita Anuwong <chanita@odd-e.com>
2014-09-19 20:38:11 +0700

    - initial db

---
+++ attack/models.py
@@ -0,0 +1,22 @@
+from pynamodb.models import Model
+from pynamodb.attributes import UnicodeAttribute, NumberAttribute, UTCDateTimeAttribute
+
+
+class Attack(Model):
+    class Meta:
+        table_name = 'Attack'
+        host = ''
+
+    attacker_ip = UnicodeAttribute(range_key=True)
+    attack_id = NumberAttribute(hash_key=True)
+
+    service_name = UnicodeAttribute()
+    protocol = UnicodeAttribute()
+    port = UnicodeAttribute()
+
+    # count = NumberAttribute(default=0)
+    longitude = UnicodeAttribute()
+    latitude = UnicodeAttribute()
+    country = UnicodeAttribute()
+    geo_location = UnicodeAttribute()
+    timestmap = UTCDateTimeAttribute()
