1da94b3a8a44b5e22ca4ff2367ab41bfaf833466
Chanita Anuwong <chanita@odd-e.com>
2014-09-23 17:54:44 +0700

    - add failing tests

--- attack/tests/test_add_attack.py
+++ attack/tests/test_add_attack.py
@@ -1,14 +1,34 @@
 from django.test import SimpleTestCase
+from attack.models import Attack
+import time
 
 
 class TestAttackAdd(SimpleTestCase):
 
+    def tearDown(self):
+        Attack.delete_table()
+        time.sleep(1)
+
+    def setUp(self):
+        if Attack.exists():
+            Attack.delete_table()
+            time.sleep(1)
+        Attack.create_table(wait=True)
+
     def test_add(self):
         fail2ban_data = dict(
-            attacker_ip='127.0.0.1',
-            service_name='company web server',
+            attacker_ip='72.14.207.99',
+            service_name='company web server test view',
             protocol='http',
-            port='80',
+            port='81',
         )
         response = self.client.post('/attack/new/', data=fail2ban_data)
+
+        attacks_from_db = Attack.query('72.14.207.99', port='81')
+        counter = 0
+        for item in attacks_from_db:
+            self.assertEqual(item.service_name, 'company web server test view')
+            self.assertEqual(item.port, '81')
+            counter += 1
+        self.assertEqual(1, counter)
         self.assertContains(response, 'Added attack')
--- attack/views.py
+++ attack/views.py
@@ -2,6 +2,6 @@ from django.http.response import HttpResponse
 
 
 def add_attack(request):
-
+    print 'ddd'
     # attack.save()
     return HttpResponse('Added attack')
