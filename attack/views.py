6597dd658f4ba5f5a0ea62154b2b03b0845912aa
Chanita Anuwong <chanita@odd-e.com>
2014-09-23 18:00:30 +0700

    - pass the tests

--- attack/views.py
+++ attack/views.py
@@ -1,7 +1,11 @@
 from django.http.response import HttpResponse
+from attack.recorder import AttackRecorder
 
 
 def add_attack(request):
-    print 'ddd'
-    # attack.save()
+    recorder = AttackRecorder()
+    recorder.set_data(**request.REQUEST.copy())
+    recorder.get_geo_data()
+    recorder.record_timestamp()
+    recorder.save()
     return HttpResponse('Added attack')
