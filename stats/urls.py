50b8151132819cdee27f91daa5d62a8454256f86
Chanita Anuwong <chanita@odd-e.com>
2014-10-01 14:50:07 +0700

    - test stats urls

--- stats/urls.py
+++ stats/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, url
 
 urlpatterns = patterns('',
-    url(r'^/$', 'attack.views.get_stats', name='get_stats'),
+    url(r'^$', 'stats.views.get_stats', name='get_stats'),
 
 )
