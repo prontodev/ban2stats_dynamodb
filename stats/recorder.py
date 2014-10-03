d041ef15197ae47d96af492552ead0bd93088a79
Chanita Anuwong <chanita@odd-e.com>
2014-10-03 19:27:55 +0700

    - recorder now can save banned ip and its test

--- stats/recorder.py
+++ stats/recorder.py
@@ -1,4 +1,59 @@
+from stats.models import BlockedIP
+from django.utils.timezone import get_current_timezone
+from datetime import datetime
+
+
 class StatsRecorder(object):
 
     def __init__(self, data):
-        pass
+        self.data = data
+        self.ip = self.data['attacker_ip']
+
+        if not BlockedIP.exists():
+            BlockedIP.create_table(wait=True)
+
+    def get_blocked_ip_category(self):
+        category = 'blocked_ip_{0}'.format(self.ip)
+        return category
+
+    def get_existing_banned_ip_record(self):
+        existing_blocked_ip = BlockedIP.query(self.ip, category=self.get_blocked_ip_category(),
+                                               service_name=self.data['service_name'], Count=True)
+        existing_records = []
+        for item in existing_blocked_ip:
+            print 'service_name = ', item.service_name
+            print 'count = ', item.count
+            existing_records.append(item)
+        print '-----'
+        return existing_records
+
+    def get_existing_banned_ip_count(self):
+        try:
+            existing_record = self.get_existing_banned_ip_record()[0]
+        except IndexError, err:
+            return 1
+        count = existing_record.count + 1
+        return count
+
+    def save_banned_ip_record(self):
+        category = self.get_blocked_ip_category()
+        count = self.get_existing_banned_ip_count()
+        last_seen = unicode(datetime.now(tz=get_current_timezone()))
+        self.blocked_ip = BlockedIP(
+            category=category,
+            key=self.ip,
+            service_name=self.data['service_name'],
+            protocol=self.data['protocol'],
+            port=self.data['port'],
+            longitude=self.data['longitude'],
+            latitude=self.data['latitude'],
+            country=self.data['country'],
+            geo_location=self.data['geo_location'],
+            count=count,
+            last_seen=last_seen,
+        )
+        self.blocked_ip.save()
+        return self.blocked_ip
+
+    def delete_table(self):
+        BlockedIP.delete_table()
--- stats/tests/test_recorder.py
+++ stats/tests/test_recorder.py
@@ -3,7 +3,7 @@ import time
 from stats.recorder import StatsRecorder
 
 
-class TestStatsRecord(SimpleTestCase):
+class TestStatsRecorder(SimpleTestCase):
 
     def test_save_stats_success(self):
         attack_data = dict(
@@ -18,3 +18,20 @@ class TestStatsRecord(SimpleTestCase):
             geo_location='Bangkok, Thailand'
         )
         recorder = StatsRecorder(attack_data)
+        banned_ip_record = recorder.save_banned_ip_record()
+        self.assertEqual(banned_ip_record.category, 'blocked_ip_127.0.0.1')
+        self.assertEqual(banned_ip_record.key, '127.0.0.1')
+        self.assertEqual(banned_ip_record.protocol, 'http')
+        self.assertEqual(banned_ip_record.port, '80')
+        self.assertEqual(banned_ip_record.service_name, 'company web server')
+        self.assertEqual(banned_ip_record.longitude, '111.333333')
+        self.assertEqual(banned_ip_record.latitude, '222.33333')
+        self.assertEqual(banned_ip_record.geo_location, 'Bangkok, Thailand')
+
+        self.assertEqual(banned_ip_record.count, 1)
+        self.assertTrue(banned_ip_record.last_seen)
+
+        banned_ip_record_2 = recorder.save_banned_ip_record()
+        self.assertEqual(banned_ip_record_2.count, 2)
+
+        recorder.delete_table()
