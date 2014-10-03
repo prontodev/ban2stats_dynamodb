eb8b11dc84621ae18c3729c60380678ec862ae14
Chanita Anuwong <chanita@odd-e.com>
2014-10-03 15:52:05 +0700

    - stats model and tests

--- stats/models.py
+++ stats/models.py
@@ -1,3 +1,52 @@
-from django.db import models
+from pynamodb.models import Model
+from pynamodb.attributes import UnicodeAttribute, NumberAttribute
 
-# Create your models here.
+
+class BlockedIP(Model):
+
+    category = UnicodeAttribute(range_key=True)
+
+    key = UnicodeAttribute(hash_key=True) #IP
+    service_name = UnicodeAttribute()
+    protocol = UnicodeAttribute()
+    port = UnicodeAttribute()
+    count = NumberAttribute()
+    last_seen = UnicodeAttribute()
+
+    longitude = UnicodeAttribute()
+    latitude = UnicodeAttribute()
+    country = UnicodeAttribute()
+    geo_location = UnicodeAttribute()
+
+    class Meta:
+        read_capacity_units = 1
+        write_capacity_units = 1
+        table_name = 'Ban2Stats_Stats'
+        region = 'ap-southeast-1'
+
+
+class AttackedProtocol(Model):
+    category = UnicodeAttribute(range_key=True, default='Attacked Protocol')
+    key = UnicodeAttribute(hash_key=True) #Protocol
+
+    count = NumberAttribute()
+
+    class Meta:
+        read_capacity_units = 1
+        write_capacity_units = 1
+        table_name = 'Ban2Stats_Stats'
+        region = 'ap-southeast-1'
+
+
+class BlockedCountry(Model):
+    category = UnicodeAttribute(range_key=True, default='Blocked Country')
+    key = UnicodeAttribute(hash_key=True) #Country code
+
+    country_name = UnicodeAttribute()
+    count = NumberAttribute()
+
+    class Meta:
+        read_capacity_units = 1
+        write_capacity_units = 1
+        table_name = 'Ban2Stats_Stats'
+        region = 'ap-southeast-1'
