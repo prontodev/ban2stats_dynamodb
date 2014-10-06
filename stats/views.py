f08dc7cac4e1ff069894831c160744e7a8d22f5c
Chanita Anuwong <chanita@odd-e.com>
2014-10-06 16:53:48 +0700

    - poc on data format for GH-pages

--- stats/views.py
+++ stats/views.py
@@ -2,4 +2,15 @@ from django.http.response import HttpResponse
 
 
 def get_stats(request):
-    return HttpResponse("var data = {}")
+    content = """
+    var blocked_ip_count = "2,777,000";
+    var blocked_countries = [
+        { country_name: "United States", count: "3,000"},
+        { country_name:"Thailand", count : "2,999"},
+        { country_name:"Singapore", count: "1,000"},
+        { country_name:"Malaysia", count: "300"},
+        { country_name:"Indonesia", count: "11"}
+    ]
+    var data = {};
+    """
+    return HttpResponse(content)
