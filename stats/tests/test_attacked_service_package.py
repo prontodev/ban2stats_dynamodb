b03dfb7d7101c6b72bab41094f54f1fcbddb81c8
Chanita Anuwong <chanita@odd-e.com>
2014-10-07 14:41:06 +0700

    - move test to new file

---
+++ stats/tests/test_attacked_service_package.py
@@ -0,0 +1,47 @@
+from django.test import SimpleTestCase
+from stats.views import AttackedServicePackage
+from stats.models import AttackedService
+import time
+
+
+class TestGetAttackedServiceStats(SimpleTestCase):
+
+    def setUp(self):
+        if not AttackedService.exists():
+            AttackedService.create_table()
+            time.sleep(1)
+        self.item1 = AttackedService(key="Internal Wordpress System", count=32923)
+        self.item1.save()
+        AttackedService(key="Mail Server", count=300).save()
+        AttackedService(key="Company Secured Server", count=127563).save()
+        self.attacked_services = AttackedServicePackage()
+
+    def test_get_attacked_services(self):
+        objects = self.attacked_services.get_objects()
+        self.assertEqual(len(objects), 3)
+
+    def test_render_each_object(self):
+        content = self.attacked_services.render_each_object(self.item1)
+        self.assertEqual(content, u'["Internal Wordpress System", 32923]')
+
+    def test_render_all_objects(self):
+        content = self.attacked_services.render_all_objects()
+        expected_content = u'''["Internal Wordpress System", 32923]'''
+        self.assertIn(expected_content, content)
+        expected_content = u'''["Mail Server", 300]'''
+        self.assertIn(expected_content, content)
+        expected_content = u'''["Company Secured Server", 127563]'''
+        self.assertIn(expected_content, content)
+
+    def test_render_javascript(self):
+        content = self.attacked_services.render_as_javascript()
+        expected_content = u'''["Internal Wordpress System", 32923]'''
+        self.assertIn(expected_content, content)
+        expected_content = u'''["Mail Server", 300]'''
+        self.assertIn(expected_content, content)
+        expected_content = u'''["Company Secured Server", 127563]'''
+        self.assertIn(expected_content, content)
+        expected_content = u'''var attacked_services = ['''
+        self.assertIn(expected_content, content)
+        expected_content = u'''];'''
+        self.assertIn(expected_content, content)
