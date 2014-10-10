5925fee77da78325c80eec151de7742828a58dac
Chanita Anuwong <chanita@odd-e.com>
2014-10-10 14:54:25 +0700

    - remove data generation in /stats/

--- stats/views.py
+++ stats/views.py
@@ -108,64 +108,11 @@ class BlockedCountryPackageBuilder(PackageBuilder):
 
 
 def get_stats(request):
-    import time
-    if not AttackedService.exists():
-        AttackedService.create_table()
-        time.sleep(1)
-    item1 = AttackedService(key="Internal Wordpress System", count=32923)
-    item1.save()
-    item1_1 = AttackedService(key="Mail Server", count=300)
-    item1_1.save()
-    item1_2 = AttackedService(key="Company Secured Server", count=127563)
-    item1_2.save()
-
-    if not BlockedIP.exists():
-        time.sleep(1)
-        BlockedIP.create_table()
-    item2 = BlockedIP("blocked_ip_72.14.207.99",
-                           key="72.14.20.99",
-                           service_name='Company Wordpress System',
-                           protocol='http',
-                           port='80',
-
-                           longitude=-122.05740356445312,
-                           latitude=37.419200897216797,
-                           country='US',
-                           geo_location='CA, United States',
-
-                           count=1000,
-                           last_seen='2014-09-27T08:49:28.556775+0000'
-                           )
-    item2.save()
-
-
-
     content = ""
     content += "\n"
     content += BlockedIPPackageBuilder().render_as_javascript()
     content += "\n"
     content += AttackedServicePackageBuilder().render_as_javascript()
-    item1.delete()
-    item1_1.delete()
-    item1_2.delete()
-    item2.delete()
-
-    if not BlockedCountry.exists():
-        BlockedCountry.create_table()
-    time.sleep(1)
-    item1 = BlockedCountry("blocked_country", key='US', country_name='United States', count=22)
-    item1.save()
-    item2 = BlockedCountry("blocked_country", key='TH', country_name='Thailand', count=3000)
-    item2.save()
-    item3 = BlockedCountry("blocked_country", key='SG', country_name='Singapore', count=12094)
-    item3.save()
-    item4 = BlockedCountry("blocked_country", key='AL', country_name='Albania', count=3)
-    item4.save()
-    item5 = BlockedCountry("blocked_country", key='MA', country_name='Morocco', count=34123)
-    item5.save()
-    item6 = BlockedCountry("blocked_country", key='PE', country_name='Peru', count=50)
-    item6.save()
     content += "\n"
     content += BlockedCountryPackageBuilder().render_as_javascript()
-
     return HttpResponse(content, content_type="application/javascript")
