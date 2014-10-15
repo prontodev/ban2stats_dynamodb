a4d0c6ebb69ca7e5d39209a7a686eee148f723cd
Chanita Anuwong <chanita@odd-e.com>
2014-10-15 15:23:21 +0700

    - clearer function name, checking matching should be in get_existing_record

--- stats/recorder.py
+++ stats/recorder.py
@@ -12,35 +12,28 @@ class StatsRecorder(object):
         if not BlockedIP.exists():
             BlockedIP.create_table(wait=True)
 
-    def trigger_counter(self, existing_record, query_data={}):
+    def trigger_counter(self, existing_record):
         if existing_record is None or (existing_record==[]): return 1
-        try:
-            existing_record = self.get_right_record(existing_record, query_data)
-        except IndexError, err:
-            return 1
-        if existing_record is None:
-            return 1
         count = existing_record.count + 1
         return count
 
-    def get_right_record(self, existing_record, query_data={}):
-        for record in existing_record:
-            match = 0
-            for query_parameter_key in query_data.keys():
-                if getattr(record, query_parameter_key) == query_data[query_parameter_key]:
-                    match += 1
-            if match == len(query_data.keys()) and (match != 0):
-                return record
+    def all_attribute_matched(self, item, query_data):
+        match = 0
+        for query_parameter_key in query_data.keys():
+            if getattr(item, query_parameter_key) == query_data[query_parameter_key]:
+                match += 1
+        if match == len(query_data.keys()) and (match != 0):
+            return True
+        return False
 
     def get_existing_record(self, hash_key, query_data):
         existing_blocked_ip = BlockedIP.query(hash_key, **query_data)
-        existing_records = []
         try:
             for item in existing_blocked_ip:
-                existing_records.append(item)
+                if self.all_attribute_matched(item, query_data):
+                    return item
         except TypeError, err:
             return None
-        return existing_records
 
     def get_blocked_ip_category(self):
         category = 'blocked_ip_{0}'.format(self.ip)
@@ -51,7 +44,7 @@ class StatsRecorder(object):
         query_data = dict(key=self.ip, service_name=self.data['service_name'])
         existing_record = self.get_existing_record(category,
                                                    query_data=query_data)
-        count = self.trigger_counter(existing_record, query_data)
+        count = self.trigger_counter(existing_record)
         last_seen = unicode(datetime.now(tz=get_current_timezone()))
         self.blocked_ip = BlockedIP(
             category=category,
@@ -70,8 +63,8 @@ class StatsRecorder(object):
         return self.blocked_ip
 
     def save_attacked_service_record(self):
-        existing_record = self.get_existing_record('attacked_service', dict(category=self.data['service_name']))
-        count = self.trigger_counter(existing_record, {'key': self.data['service_name']})
+        existing_record = self.get_existing_record('attacked_service', dict(key=self.data['service_name']))
+        count = self.trigger_counter(existing_record)
         self.attacked_service = AttackedService(key=self.data['service_name'], count=count)
         self.attacked_service.save()
         return self.attacked_service
@@ -79,7 +72,7 @@ class StatsRecorder(object):
     def save_blocked_country_record(self):
         query_data = dict(key=self.data['country'])
         existing_record = self.get_existing_record('blocked_country', query_data)
-        count = self.trigger_counter(existing_record, query_data=query_data)
+        count = self.trigger_counter(existing_record)
         self.blocked_country = BlockedCountry(key=self.data['country'],
                                               country_name=self.data['country_name'],
                                               count=count)
