90d2b81ad2486ff0b159d9eb58319564ca6c6799
Chanita Anuwong <chanita@odd-e.com>
2014-10-20 17:34:52 +0700

    - new apis for each data

--- stats/views.py
+++ stats/views.py
@@ -13,6 +13,25 @@ def get_stats(request):
     content += ",\n"
     content += BlockedCountryPackageBuilder().render_as_javascript()
     content += "\n}"
+    return return_json_content(content)
+
+
+def return_json_content(content):
     response = HttpResponse(content, content_type="application/json")
     response["Access-Control-Allow-Origin"] = "*"
     return response
+
+
+def get_attacked_services(request):
+    content = AttackedServicePackageBuilder().render_all_objects_as_list()
+    return return_json_content(content)
+
+
+def get_blocked_countries(request):
+    content = BlockedCountryPackageBuilder().render_all_objects_as_list()
+    return return_json_content(content)
+
+
+def get_blocked_ips(request):
+    content = BlockedIPPackageBuilderMinimized().render_all_objects_as_list()
+    return return_json_content(content)
