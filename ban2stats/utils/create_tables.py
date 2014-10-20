1665e91378a65494a9db8177b20e6a6333b0f3a2
Chanita Anuwong <chanita@odd-e.com>
2014-10-20 13:48:11 +0700

    - create table now have to create all 4

--- ban2stats/utils/create_tables.py
+++ ban2stats/utils/create_tables.py
@@ -1,5 +1,5 @@
 from attack.models import Attack
-from stats.models import BlockedIP
+from stats.models import BlockedIP, BlockedCountry, AttackedService
 
 
 def create_all():
@@ -8,3 +8,9 @@ def create_all():
 
     if not BlockedIP.exists():
         BlockedIP.create_table(wait=True)
+
+    if not BlockedCountry.exists():
+        BlockedCountry.create_table()
+
+    if not AttackedService.exists():
+        AttackedService.create_table()
